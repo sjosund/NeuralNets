c7d749b7c25cc8afb40baeb01ac9564e73d17866
Lars Sjosund <lars@peltarion.com>
2017-05-02 20:20:37 +0200

    Working style transfer

--- nn/style_transfer.py
+++ nn/style_transfer.py
@@ -1,6 +1,7 @@
 import os
 from pprint import pprint
 
+import time
 from keras.applications import vgg16
 from keras import backend as K
 from keras.preprocessing.image import load_img, img_to_array
@@ -14,78 +15,83 @@ base_dir = os.path.join(
 )
 data_dir = os.path.join(base_dir, 'data')
 log_dir = os.path.join(base_dir, 'logs')
-style_path = os.path.join(data_dir, 'shipwreck.jpg')
 content_path = os.path.join(data_dir, 'tubingen.jpg')
-results_dir = os.path.join(base_dir, 'results', 'style_transfer')
+shape = (1, 384, 512, 3)
+
+content_weight = 1e3
+style_weight = 1e7
+tv_weight = 0
 
 
 def main():
-    shape = (1, 200, 300, 3)
-
-    style_image = load_image(style_path, shape)
-    style_image_tensor = tf.Variable(
-        style_image,
-        dtype=tf.float32
-    )
-    imsave(
-        '{}/style.jpg'.format(results_dir),
-        deprocess_image(style_image, shape)
-    )
-
-    content_image = load_image(content_path, shape)
-    content_image_tensor = tf.Variable(
-        content_image,
-        dtype=tf.float32
-    )
-
-    imsave(
-        '{}/content.jpg'.format(results_dir),
-        deprocess_image(content_image, shape)
-    )
-
-    noise_ratio = 0.6
-    combination_image = tf.Variable(
-        content_image * (1 - noise_ratio) + noise_ratio * np.random.uniform(
-            low=-20,
-            high=20,
-            size=shape
-        ),
-        dtype=tf.float32
-    )
-    input_tensor = tf.concat(
-        values=[
-            style_image_tensor,
-            content_image_tensor,
-            combination_image
-        ],
-        axis=0
-    )
-
-    model = vgg16.VGG16(
-        include_top=False,
-        weights='imagenet',
-        input_tensor=input_tensor
-    )
-
-    output_dict = {l.name: l.output for l in model.layers}
-    pprint(output_dict.keys())
-
-    loss = total_loss(output_dict)
-    train_step = tf.train.AdamOptimizer(learning_rate=2.0).minimize(
-        loss,
-        var_list=[combination_image]
-    )
-
-    n_epochs = 1000
-
-    merged_summary = tf.summary.merge_all()
 
     with tf.Session() as sess:
+        K.set_session(sess)
+
+        if not os.path.exists(results_dir):
+            os.mkdir(results_dir)
+        style_image = load_image(style_path, shape)
+        style_image_tensor = tf.Variable(
+            style_image,
+            dtype=tf.float32
+        )
+        imsave(
+            '{}/style.jpg'.format(results_dir),
+            deprocess_image(style_image, shape)
+        )
+
+        content_image = load_image(content_path, shape)
+        content_image_tensor = tf.Variable(
+            content_image,
+            dtype=tf.float32
+        )
+
+        imsave(
+            '{}/content.jpg'.format(results_dir),
+            deprocess_image(content_image, shape)
+        )
+
+        combination_image = tf.Variable(
+            content_image + tf.random_normal(mean=1, stddev=0.01, shape=shape),
+            dtype=tf.float32
+        )
+        input_tensor = tf.concat(
+            values=[
+                style_image_tensor,
+                content_image_tensor,
+                combination_image
+            ],
+            axis=0
+        )
+
+        model = vgg16.VGG16(
+            include_top=False,
+            weights='imagenet',
+            input_tensor=input_tensor
+        )
+
+        output_dict = {l.name: l.output for l in model.layers}
+        pprint(output_dict.keys())
+
+        loss = total_loss(output_dict, combination_image)
+        train_step = tf.train.AdamOptimizer(learning_rate=10.0).minimize(
+            loss,
+            var_list=[combination_image]
+        )
+
+        n_epochs = 1000
+
+        merged_summary = tf.summary.merge_all()
+
         train_writer = tf.summary.FileWriter(
-            '{}/train'.format(log_dir),
+            '{}/train_{}'.format(
+                log_dir,
+                int(time.time())
+            ),
             sess.graph
         )
-        sess.run(tf.global_variables_initializer())
+        # sess.run(tf.global_variables_initializer())
+        initialize_variables()
         for i in range(n_epochs):
             print('Starting epoch {}'.format(i))
             summary, loss_value, _ = sess.run(
@@ -93,12 +99,16 @@ def main():
             )
             train_writer.add_summary(summary, i)
             print(loss_value)
-            if i % 10 == 0:
+            if i % 100 == 0:
                 img = deprocess_image(
                     sess.run(combination_image),
                     shape=shape
                 )
                 imsave('{}/res_iter_{}.jpg'.format(results_dir, i), img)
+                img = deprocess_image(
+                    sess.run(style_image_tensor),
+                    shape=shape
+                )
         final_image = deprocess_image(
             sess.run(combination_image),
             shape=shape
@@ -115,9 +125,7 @@ def load_image(path, shape):
     return img
 
 
-def total_loss(output_dict):
-    content_weight = 5e0
-    style_weight = 1e4
+def total_loss(output_dict, image):
     loss = tf.Variable(0.)
     content_loss_ = content_weight * content_loss(output_dict)
     tf.summary.scalar('content_loss', content_loss_)
@@ -127,6 +135,11 @@ def total_loss(output_dict):
     tf.summary.scalar('style_loss', style_loss_)
     loss += style_loss_
 
+    tv_loss = tv_weight * total_variation_loss(image, image_shape=shape)
+    tf.summary.scalar('tv_loss', tv_loss)
+    loss += tv_loss
+    tf.summary.scalar('total_loss', loss)
+
     return loss
 
 
@@ -135,12 +148,8 @@ def content_loss(output_dict):
     content_features = output_dict[content_layer][1, ...]
     combined_features = output_dict[content_layer][2, ...]
 
-    N_l = tf.cast(content_features.shape[2], tf.float32)
-    M_l = tf.cast(content_features.shape[0] * content_features.shape[1], tf.float32)
-
-    content_loss_ = tf.reduce_sum(
-        tf.square(content_features - combined_features) / (4 * N_l * M_l)
-    )
+    squared_diff = tf.square(content_features - combined_features)
+    content_loss_ = tf.reduce_sum(squared_diff) / 2
 
     return content_loss_
 
@@ -166,11 +175,29 @@ def style_loss(output_dict):
             gram_matrix(combined_image_layer_output)
         )
         tf.summary.histogram('gram_{}'.format(layer), squared_gram_diff)
-        style_loss_ += layer_weights[i] / (4 * N_l**2 * M_l**2) * tf.reduce_sum(
+        style_loss_ += layer_weights[i] * tf.reduce_sum(
             squared_gram_diff
-        )
+        ) / (4 * N_l**2 * M_l**2)
 
     return style_loss_
+
+
+def total_variation_loss(image, image_shape=None):
+    if image_shape:
+        _, width, height, channels = image_shape
+    else:
+        # Try to get the image size from the tensor
+        dims = image.get_shape()
+        width = dims[1].value
+        height = dims[2].value
+        channels = dims[3].value
+
+    tv_x_size = width * (height - 1) * channels
+    tv_y_size = (width - 1) * height * channels
+    return (
+        tf.reduce_sum(tf.abs(image[:, 1:, :, :] - image[:, :-1, :, :])) / tv_x_size +
+        tf.reduce_sum(tf.abs(image[:, :, 1:, :] - image[:, :, :-1, :])) / tv_y_size
+    )
 
 
 def gram_matrix(x):
@@ -193,5 +220,59 @@ def deprocess_image(x, shape):
     return x
 
 
+def get_uninitialized_variables(variables=None, session=None):
+    """
+    Get uninitialized variables in a session as a list.
+        Args:
+            variables: list of tf.Variable. Get uninitiliazed vars
+                from these. If none, gets all uinitialized vars in session.
+            session: tf.Session to find uninitialized vars in. If none
+                uses default session.
+        Returns:
+            Uninitialized variables within `variables`.
+            If `variables` not specified, return all uninitialized variables.
+    """
+    if not session:
+        session = tf.get_default_session()
+    if variables is None:
+        variables = tf.global_variables()
+    else:
+        variables = list(variables)
+    init_flag = session.run(
+        tf.stack([tf.is_variable_initialized(v) for v in variables]))
+    return [v for v, f in zip(variables, init_flag) if not f]
+
+
+def initialize_variables():
+    '''Initialize the internal variables the optimizer uses.
+        We could do tf.global_variables_initializer().eval() to
+        initialize all variables but this messes up the keras model.'''
+    # Get uninitialized vars and their initializers
+    uninitialized_vars = get_uninitialized_variables()
+    initializers = [var.initializer for var in uninitialized_vars]
+
+    # Print uninitialized variables
+    print('Uninitialized variables:')
+    print([initializer.name for initializer in initializers])
+
+    # Initialize the variables
+    _ = [initializer.run() for initializer in initializers]
+
+
 if __name__ == '__main__':
-    main()
+    names = ['seated-nude', 'Composition-VII', 'shipwreck',
+             'starry_night', 'the_scream']
+    for name in names:
+        style_path = os.path.join(data_dir, '{}.jpg'.format(name))
+        results_dir = os.path.join(
+            base_dir,
+            'results',
+            'style_transfer',
+            '{}_style{}_content{}_tv{}'.format(
+                name,
+                style_weight,
+                content_weight,
+                tv_weight
+            )
+        )
+        main()
